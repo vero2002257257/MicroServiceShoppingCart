05e798cea84a764ac49639170442343bac049e8e
veronica <105020892+vero2002257257@users.noreply.github.com>
2024-10-11 17:20:27 -0500

    Creación del repositorio

---
+++ src/main/java/Emazon/MicroServiceShopCart/MicroServiceShopCartApplication.java
@@ -0,0 +1,13 @@
+package Emazon.MicroServiceShopCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceShopCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceShopCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Emazon/MicroServiceShopCart/MicroServiceShopCartApplicationTests.java
@@ -0,0 +1,13 @@
+package Emazon.MicroServiceShopCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceShopCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
